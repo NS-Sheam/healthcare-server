5da769837cee98f9c68723fbfe0bebc4a00a0434
NS-Sheam <123sheamfeni@gmail.com>
2024-03-17 15:07:42 +0600

    Add filters to getAllAdminFromDB function

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -1,10 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
 import { AdminService } from "./admin.service";
+import pick from "../../../shared/pick";
 
 const getAllAdminFromDB = async (req: Request, res: Response) => {
+  const filters = pick(req.query, ["searchTerm", "name", "email", "contactNumber"]);
+
   try {
-    const result = await AdminService.getAllAdminFromDB(req.query);
+    const result = await AdminService.getAllAdminFromDB(filters);
     res.status(200).json({
       success: true,
       message: "Admins fetched successfully",
