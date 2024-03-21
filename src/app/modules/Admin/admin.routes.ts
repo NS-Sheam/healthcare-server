8d5f0cbb31b2662384e99435a35694cef88c7a99
NS-Sheam <123sheamfeni@gmail.com>
2024-03-21 20:33:08 +0600

    Add zod library for input validation

--- src/app/modules/Admin/admin.routes.ts
+++ src/app/modules/Admin/admin.routes.ts
@@ -1,10 +1,30 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { AdminController } from "./admin.controller";
+import { AnyZodObject, z } from "zod";
 
 const router = express.Router();
+
+const update = z.object({
+  body: z.object({
+    name: z.string().optiona(),
+    contactNumber: z.string().optional(),
+  }),
+});
+
+const validateRequest = (schema: AnyZodObject) => async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    await schema.parseAsync({
+      body: req.body,
+    });
+    return next();
+  } catch (error) {
+    next(error);
+  }
+};
+
 router.get("/", AdminController.getAllAdminFromDB);
 router.get("/:id", AdminController.getByIdFromDB);
-router.patch("/:id", AdminController.updateIntoDB);
+router.patch("/:id", validateRequest(update), AdminController.updateIntoDB);
 router.delete("/:id", AdminController.deleteFromDB);
 router.patch("/soft/:id", AdminController.softDeleteFromDB);
 
