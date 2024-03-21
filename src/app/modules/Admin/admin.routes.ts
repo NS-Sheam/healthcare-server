919d60062343bd7c7226dc560b7e98134d094553
NS-Sheam <123sheamfeni@gmail.com>
2024-03-21 20:36:19 +0600

    Refactor admin.routes.ts to use separate validation schemas

--- src/app/modules/Admin/admin.routes.ts
+++ src/app/modules/Admin/admin.routes.ts
@@ -1,30 +1,14 @@
-import express, { NextFunction, Request, Response } from "express";
+import express from "express";
 import { AdminController } from "./admin.controller";
-import { AnyZodObject, z } from "zod";
+
+import validateRequest from "../../middlewares/validateRequest";
+import { adminValidationSchemas } from "./admin.validation";
 
 const router = express.Router();
 
-const update = z.object({
-  body: z.object({
-    name: z.string().optional(),
-    contactNumber: z.string().optional(),
-  }),
-});
-
-const validateRequest = (schema: AnyZodObject) => async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    await schema.parseAsync({
-      body: req.body,
-    });
-    return next();
-  } catch (error) {
-    next(error);
-  }
-};
-
 router.get("/", AdminController.getAllAdminFromDB);
 router.get("/:id", AdminController.getByIdFromDB);
-router.patch("/:id", validateRequest(update), AdminController.updateIntoDB);
+router.patch("/:id", validateRequest(adminValidationSchemas.update), AdminController.updateIntoDB);
 router.delete("/:id", AdminController.deleteFromDB);
 router.patch("/soft/:id", AdminController.softDeleteFromDB);
 
