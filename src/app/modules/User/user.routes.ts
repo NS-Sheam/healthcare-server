a43ae7f14afd64464c913c21e8575d39d5b3c7f2
NS-Sheam <123sheamfeni@gmail.com>
2024-03-22 23:12:05 +0600

    adding apierror

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -1,7 +1,10 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import { userController } from "./user.controller";
+import { UserRole } from "@prisma/client";
+
+import auth from "../../middlewares/auth";
 const router = express.Router();
 
-router.post("/", userController.createAdmin);
+router.post("/", auth(UserRole.ADMIN, UserRole.SUPER_ADMIN), userController.createAdmin);
 
 export const userRoutes = router;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import { Server } from "http";
 import app from "./app";
+import config from "./config";
 
-const port = 3000;
 async function main() {
-  const server: Server = app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+  const server: Server = app.listen(config.port, () => {
+    console.log(`Server is running at http://localhost:${config.port}`);
   });
 }
 
