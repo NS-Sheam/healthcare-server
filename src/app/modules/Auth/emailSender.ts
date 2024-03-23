5e295e9510f99743235cc4de7184802a10589b29
NS-Sheam <123sheamfeni@gmail.com>
2024-03-23 12:51:10 +0600

    Update emailSender configuration

--- src/app/modules/Auth/emailSender.ts
+++ src/app/modules/Auth/emailSender.ts
@@ -1,21 +1,22 @@
 import nodemailer from "nodemailer";
 import config from "../../../config";
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.ethereal.email",
-  port: 587,
-  secure: false, // Use `true` for port 465, `false` for all other ports
-  auth: {
-    user: config.emailSender.email,
-    pass: config.emailSender.app_password,
-  },
-});
+const emailSender = async () => {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // Use `true` for port 465, `false` for all other ports
+    auth: {
+      user: config.emailSender.email,
+      pass: config.emailSender.app_password,
+    },
+  });
 
-// async..await is not allowed in global scope, must use a wrapper
-async function main() {
+  // async..await is not allowed in global scope, must use a wrapper
+
   // send mail with defined transport object
   const info = await transporter.sendMail({
-    from: '"Maddison Foo Koch 👻" <maddison53@ethereal.email>', // sender address
+    from: '"Health Care" <123sheamfeni@gmail.com>', // sender address
     to: "bar@example.com, baz@example.com", // list of receivers
     subject: "Hello ✔", // Subject line
     text: "Hello world?", // plain text body
@@ -23,7 +24,6 @@ async function main() {
   });
 
   console.log("Message sent: %s", info.messageId);
-  // Message sent: <d786aa62-4e0a-070a-47ed-0b0666549519@ethereal.email>
-}
+};
 
-main().catch(console.error);
+export default emailSender;
