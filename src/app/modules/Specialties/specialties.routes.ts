20a13a89bef9f2438d395834bf7e8159922e6d29
NS-Sheam <123sheamfeni@gmail.com>
2024-04-01 21:05:04 +0600

    Add specialties validation to specialties.routes.ts

--- src/app/modules/Specialties/specialties.routes.ts
+++ src/app/modules/Specialties/specialties.routes.ts
@@ -1,11 +1,12 @@
 import express, { NextFunction, Request, Response } from "express";
 import { SpecialtiesController } from "./specialties.controller";
 import { fileUploader } from "../../../helpers/fileUploader";
+import { specialtiesValidation } from "./specialties.validation";
 
 const router = express.Router();
 
 router.post("/", fileUploader.upload.single("file"), (req: Request, res: Response, next: NextFunction) => {
-  req.body = JSON.parse(req.body.data);
+  req.body = specialtiesValidation.createSpecialtyValidationSchema.parse(JSON.parse(req.body.data));
   return SpecialtiesController.insertSpecialtiesIntoDB(req, res, next);
 });
 
