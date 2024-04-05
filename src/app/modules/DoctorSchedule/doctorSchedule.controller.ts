3622aede5547d668f588d2ecf841144470e698dc
NS-Sheam <123sheamfeni@gmail.com>
2024-04-05 20:51:04 +0600

    Add getMySchedules endpoint

--- src/app/modules/DoctorSchedule/doctorSchedule.controller.ts
+++ src/app/modules/DoctorSchedule/doctorSchedule.controller.ts
@@ -3,6 +3,7 @@ import catchAsync from "../../../shared/catchAsync";
 import { DoctorScheduleService } from "./doctorSchedule.service";
 import sendResponse from "../../../shared/sendResponse";
 import { IAuthUser } from "../../interfaces/common";
+import pick from "../../../shared/pick";
 
 const insertIntoDB = catchAsync(async (req: Request & { user?: IAuthUser }, res: Response) => {
   const result = await DoctorScheduleService.insertIntoDB(req.user, req.body);
@@ -13,7 +14,22 @@ const insertIntoDB = catchAsync(async (req: Request & { user?: IAuthUser }, res:
     data: result,
   });
 });
+const getMySchedules = catchAsync(async (req: Request & { user?: IAuthUser }, res: Response) => {
+  const user = req.user;
+  const filter = pick(req.query, ["startDate", "endDate"]);
+
+  const options = pick(req.query, ["limit", "page", "sortBy", "sortOrder"]);
+
+  const result = await DoctorScheduleService.getMySchedules(filter, options, user as IAuthUser);
+  sendResponse(res, {
+    status: 200,
+    success: true,
+    message: "Doctor Schedules fetched successfully",
+    data: result,
+  });
+});
 
 export const DoctorScheduleController = {
   insertIntoDB,
+  getMySchedules,
 };
--- src/app/modules/DoctorSchedule/doctorSchedule.service.ts
+++ src/app/modules/DoctorSchedule/doctorSchedule.service.ts
@@ -1,4 +1,8 @@
+import { Prisma } from "@prisma/client";
+import { paginationHelper } from "../../../helpers/paginationHelper";
 import prisma from "../../../shared/prisma";
+import { IAuthUser } from "../../interfaces/common";
+import { IPaginationOptions } from "../../interfaces/pagination";
 
 const insertIntoDB = async (
   user: any,
@@ -24,6 +28,87 @@ const insertIntoDB = async (
   return result;
 };
 
+const getMySchedules = async (param: any, options: IPaginationOptions, user: IAuthUser) => {
+  const { page, limit, skip } = paginationHelper.calculatePagination(options);
+  const { startDate, endDate, ...filteredData } = param;
+
+  const andCondition: Prisma.ScheduleWhereInput[] = [];
+  if (startDate && endDate) {
+    andCondition.push({
+      AND: [
+        {
+          startDateTime: {
+            gte: new Date(startDate),
+          },
+        },
+        {
+          endDateTime: {
+            lte: new Date(endDate),
+          },
+        },
+      ],
+    });
+  }
+  if (Object.keys(filteredData).length > 0) {
+    andCondition.push({
+      AND: Object.keys(filteredData).map((key) => ({
+        [key]: {
+          equals: (filteredData as any)[key],
+        },
+      })),
+    });
+  }
+
+  const whereCondition: Prisma.ScheduleWhereInput = { AND: andCondition };
+
+  const doctorSchedules = await prisma.doctorSchedules.findMany({
+    where: {
+      doctor: {
+        email: user?.email,
+      },
+    },
+  });
+  const doctorScheduleIds = doctorSchedules.map((schedule) => schedule.scheduleId);
+
+  const result = await prisma.schedule.findMany({
+    where: {
+      ...whereCondition,
+      id: {
+        notIn: doctorScheduleIds,
+      },
+    },
+    skip,
+    take: limit,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? {
+            [options.sortBy]: options.sortOrder,
+          }
+        : {
+            createdAt: "desc",
+          },
+  });
+
+  const total = await prisma.schedule.count({
+    where: {
+      ...whereCondition,
+      id: {
+        notIn: doctorScheduleIds,
+      },
+    },
+  });
+
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+    },
+    data: result,
+  };
+};
+
 export const DoctorScheduleService = {
   insertIntoDB,
+  getMySchedules,
 };
