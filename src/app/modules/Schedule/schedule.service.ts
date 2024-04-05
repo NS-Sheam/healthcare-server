2b07c79509665b8f73510f586d5abad4997b634c
NS-Sheam <123sheamfeni@gmail.com>
2024-04-05 15:49:03 +0600

    add schedule

--- src/app/modules/Schedule/schedule.service.ts
+++ src/app/modules/Schedule/schedule.service.ts
@@ -1,13 +1,30 @@
-import { addHours, format } from "date-fns";
+import { addHours, addMinutes, format } from "date-fns";
+import prisma from "../../../shared/prisma";
 const insertScheduleIntoDB = async (payload: any) => {
   const { startDate, endDate, startTime, endTime } = payload;
-  const currentDate = new Date(startDate);
-  const lastDate = new Date(endDate);
+  const intervelTime = 30;
+  const schedules = [];
+
+  const currentDate = new Date(startDate); // Start Date
+  const lastDate = new Date(endDate); // End Date
   while (currentDate <= lastDate) {
     const startDateTime = new Date(addHours(`${format(currentDate, "yyyy-MM-dd")}`, Number(startTime.split(":")[0])));
-    const endDateTime = new Date(addHours(`${format(lastDate, "yyyy-MM-dd")}`, Number(endTime.split(":")[0])));
-    console.log(endDateTime);
+    const endDateTime = new Date(addHours(`${format(currentDate, "yyyy-MM-dd")}`, Number(endTime.split(":")[0])));
+
+    while (startDateTime < endDateTime) {
+      const scheduleData = {
+        startDateTime: startDateTime,
+        endDateTime: addMinutes(startDateTime, intervelTime),
+      };
+      const result = await prisma.schedule.create({
+        data: scheduleData,
+      });
+      schedules.push(result);
+      startDateTime.setMinutes(startDateTime.getMinutes() + intervelTime);
+    }
+    currentDate.setDate(currentDate.getDate() + 1);
   }
+  return schedules;
 };
 
 export const ScheduleService = {
