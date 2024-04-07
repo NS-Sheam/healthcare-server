a0d90bea90c725bcc63376677d502c178e6bde70
NS-Sheam <123sheamfeni@gmail.com>
2024-04-07 13:14:08 +0600

    add appointment service

--- src/app/modules/Appointment/appointment.controller.ts
+++ src/app/modules/Appointment/appointment.controller.ts
@@ -3,13 +3,15 @@ import catchAsync from "../../../shared/catchAsync";
 import { AppointmentService } from "./appointment.service";
 import httpStatus from "http-status";
 import sendResponse from "../../../shared/sendResponse";
+import { IAuthUser } from "../../interfaces/common";
 
-const createAppointment = catchAsync(async (req: Request, res: Response) => {
-  const result = await AppointmentService.createAppointment();
+const createAppointment = catchAsync(async (req: Request & { user?: IAuthUser }, res: Response) => {
+  const user = req.user;
+  const result = await AppointmentService.createAppointment(user as IAuthUser, req.body);
   sendResponse(res, {
     status: httpStatus.OK,
     success: true,
-    message: "Appointment created successfully",
+    message: "Appointment booked successfully",
     data: result,
   });
 });
