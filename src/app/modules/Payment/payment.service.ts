a62a1bd757086bdf733cad370efb2775087b298b
NS-Sheam <123sheamfeni@gmail.com>
2024-04-14 13:20:41 +0600

    Update Payment model in schema.prisma and related code

--- src/app/modules/Payment/payment.service.ts
+++ src/app/modules/Payment/payment.service.ts
@@ -32,25 +32,38 @@ const initPayment = async (appointmentId: string) => {
     paymentUrl: result.GatewayPageURL,
   };
 };
-// amount=1150.00&bank_tran_id=151114130739MqCBNx5&card_brand=VISA&card_issuer=BRAC+BANK%2C+LTD.&card_issuer_country=Bangladesh&card_issuer_country_code=BD&card_no=432149XXXXXX0667&card_type=VISA-Brac+bank¤cy=BDT&status=VALID&store_amount=1104.00&store_id=ncomm65f078be011ae&tran_date=2015-11-14+13%3A07%3A12&tran_id=5646dd9d4b484&val_id=151114130742Bj94IBUk4uE5GRj&verify_sign=345e55054e9c3572d1bfbf2c0aa29afc&verify_key=amount%2Cbank_tran_id%2Ccard_brand%2Ccard_issuer%2Ccard_issuer_country%2Ccard_issuer_country_code%2Ccard_no%2Ccard_type%2Ccurrency%2Cstatus%2Cstore_amount%2Cstore_id%2Ctran_date%2Ctran_id%2Cval_id
+
 const validatePayment = async (payload: any) => {
-  const response = await SSLService.validatePayment(payload);
-  if (response.status !== "VALID") {
-    return {
-      message: "Payment failed",
-    };
-  }
+  // const response = await SSLService.validatePayment(payload);
+  // if (response.status !== "VALID") {
+  //   return {
+  //     message: "Payment failed",
+  //   };
+  // }
+  const response = payload;
+
   await prisma.$transaction(async (tx) => {
-    await tx.payment.updateMany({
+    const updatedPaymentdata = await tx.payment.update({
       where: {
         transactionId: payload.tran_id,
       },
       data: {
         status: PaymentStatus.PAID,
-        paymentGatewayData: response.data,
+        paymentGatewayData: response,
+      },
+    });
+    await tx.appointment.update({
+      where: {
+        id: updatedPaymentdata.appointmentId,
+      },
+      data: {
+        paymentStatus: PaymentStatus.PAID,
       },
     });
   });
+  return {
+    message: "Payment successful",
+  };
 };
 export const PaymentService = {
   initPayment,
