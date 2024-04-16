91916dc982eafbc48bac8e858d0492c95deb923a
NS-Sheam <123sheamfeni@gmail.com>
2024-04-17 00:22:56 +0600

    refector glovalErrorHandler

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,11 +1,28 @@
+import { Prisma } from "@prisma/client";
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status";
 
 const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-  res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-    success: false,
-    message: err?.message || "Something went wrong!",
-    error: err,
+  let statusCode = err.statusCode || httpStatus.INTERNAL_SERVER_ERROR;
+  let success = false;
+  let message = err.message || "Something went wrong!";
+  let error = err;
+  if (err instanceof Prisma.PrismaClientValidationError) {
+    statusCode = httpStatus.BAD_REQUEST;
+    message = "Validation Error";
+    error = err.message;
+  } else if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    if (err.code === "P2002") {
+      statusCode = httpStatus.CONFLICT;
+      message = "Duplicate key error";
+      error = err.meta;
+    }
+  }
+
+  res.status(statusCode).json({
+    success,
+    message,
+    error,
   });
 };
 
