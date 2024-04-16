0e8f707baf47f7572b4cbfa03167308e2cc73235
NS-Sheam <123sheamfeni@gmail.com>
2024-04-17 02:02:30 +0600

    add prisma logging

--- src/shared/prisma.ts
+++ src/shared/prisma.ts
@@ -1,5 +1,52 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  log: [
+    { level: "warn", emit: "event" },
+    { level: "info", emit: "event" },
+    { level: "error", emit: "event" },
+  ],
+});
+
+prisma.$on("warn", (e) => {
+  console.log(e);
+});
+
+prisma.$on("info", (e) => {
+  console.log(e);
+});
+
+prisma.$on("error", (e) => {
+  console.log(e);
+});
+// const prisma = new PrismaClient({
+//   log: [
+//     {
+//       emit: "event",
+//       level: "query",
+//     },
+//     {
+//       emit: "stdout",
+//       level: "error",
+//     },
+//     {
+//       emit: "stdout",
+//       level: "info",
+//     },
+//     {
+//       emit: "stdout",
+//       level: "warn",
+//     },
+//   ],
+// });
+
+// prisma.$on("query", (e) => {
+//   console.log("Query: " + e.query);
+//   console.log("------------------------------------");
+//   console.log("Params: " + e.params);
+//   console.log("------------------------------------");
+//   console.log("Duration: " + e.duration + "ms");
+//   console.log("--------------------------------------------------");
+// });
 
 export default prisma;
