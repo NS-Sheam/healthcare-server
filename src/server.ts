beb6c41933d3caf65d8a402465104409a46d706e
NS-Sheam <123sheamfeni@gmail.com>
2024-04-17 17:43:37 +0600

    Add error handling for uncaught exceptions and unhandled rejections in server.ts

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,24 @@ async function main() {
   const server: Server = app.listen(config.port, () => {
     console.log(`Server is running at http://localhost:${config.port}`);
   });
+
+  const exitHandler = () => {
+    if (server) {
+      server.close(() => {
+        console.log("Server closed!");
+      });
+    }
+    process.exit(1);
+  };
+
+  process.on("uncaughtException", (error) => {
+    console.error(error);
+    exitHandler();
+  });
+  process.on("unhandledRejection", (error) => {
+    console.error(error);
+    exitHandler();
+  });
 }
 
 main();
